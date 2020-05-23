66e35901d4d42ff5aebdb42b7994416d6b46d08f
zhangjies <1450443957@qq.com>
2020-05-23 11:46:10 +0800

    vue组件开入门

--- app.vue
+++ app.vue
@@ -1,12 +1,28 @@
 <template>
-    <div>Hello,{{name}} </div>
+   <div>
+      <v-ttitle title="vue组件化开发"></v-ttitle>
+       <v-button @click1="handleClick">点击按钮</v-button>
+       <div>Hello,{{name}} </div>
+   </div>
+
 </template>
 
 <script>
+    import vTtitle from './title.vue';
+    import vButton from './button.vue';
     export default {
+        components:{
+            vTtitle,
+            vButton
+        },
         data(){
             return {
                 name: "vue.js"
+            }
+        },
+        methods:{
+            handleClick(e){
+                console.log(e)
             }
         }
 
